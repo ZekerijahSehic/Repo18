c97eb6d7364f62990177609007b2c46a716f25da
Zekerijah <zekerijahsehic1@gmail.com>
2021-10-01 10:23:06 +0200

    Generator Functions

---
+++ ex01/src/index.js
@@ -0,0 +1,20 @@
+let myArr = [];
+// Only change code below this line
+function* multiplication(x) {
+    for (let i = 0; i <= x; i++) {
+        x = x * 2;
+        myArr.push(x);
+        yield x;
+        
+    }
+}
+
+console.log(myArr);
+
+var num = multiplication(3);
+
+for (let i = 0; i < 3; i++) {
+    console.log(num.next().value)
+}
+// Only change code above this line
+module.exports = multiplication;
